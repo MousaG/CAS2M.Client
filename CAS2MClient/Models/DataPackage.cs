6cc672321f809abb3629f9daa0d60471e99c76e0
mousa golmiri <mgolmiri@gmail.com>
2017-10-31 16:28:05 +0330

    4rd commi

--- CAS2MClient/Models/DataPackage.cs
+++ CAS2MClient/Models/DataPackage.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using CAS2MClientDataMan.Enums;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
+//using CAS2MClientDataMan.Enums;
 
-namespace CAS2MClient.Models
-{
-    public class DataPackage<T>
-    {
-        public EntityType entityType;
+//namespace CAS2MClient.Models
+//{
+//    public class DataPackage<T>
+//    {
+//        public EntityType entityType;
 
-        public Guid taskToken { get; set; }
-        public Uri callbackUrl { get; set; }
-        public List<T> data { get; set; }
-    }
-}
+//        public Guid taskToken { get; set; }
+//        public Uri callbackUrl { get; set; }
+//        public List<T> data { get; set; }
+//    }
+//}
