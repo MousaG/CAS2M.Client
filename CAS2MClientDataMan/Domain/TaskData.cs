d80da646208e7ae752c69c0b5a74142935c34a21
mousa golmiri <mgolmiri@gmail.com>
2017-11-01 10:17:03 +0330

    set task added

---
+++ CAS2MClientDataMan/Domain/TaskData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAS2MClientDataMan.Domain
+{
+   public class TaskData
+    {
+        public bool result{ get; set; }
+        public string message{ get; set; }
+        public string taskid { get; set; }
+        public Guid taskToken { get; set; }
+        public string callbackurl { get; set; }
+    }
+}
